dcc8a968eb3027e7b43f2e615ec76c6929fe00b9
4lfernandes <48036343+4lfernandes@users.noreply.github.com>
2021-07-04 14:24:40 -0300

    Update Processador.java
    Foi implementado o método que faz o pagamento do boleto na fatura, agora o caso de teste anterior é concluido com sucesso

--- Processador.java
+++ Processador.java
@@ -8,7 +8,24 @@ public class Processador {
 			
 	}
 	
-	
+	public boolean pagamentoProcessador(Boleto bol, Fatura fat) {
+		
+		double boleto = bol.getValorPago();
+		System.out.println("O valor da fatura era "+ fat.getValorFatura());
+		fat.setValorFatura(fat.getValorFatura()-boleto);
+		fat.verificaDivida();
+		System.out.println("O valor da fatura agora é: "+ fat.getValorFatura());
+		if (fat.pago == true) {
+			
+			System.out.println("A fatura está paga");
+			
+		}else {
+			
+			System.out.println("A fatura não está paga");
+			
+		}
+		
+		return true;
 		
 	
 	
