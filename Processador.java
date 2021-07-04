1a173ec7e718752f42270eb879e5f7c778ae77a5
4lfernandes <48036343+4lfernandes@users.noreply.github.com>
2021-07-04 14:26:39 -0300

    Update Processador.java
    Refatoração feita no código, removidos alguns prints que estavam sendo chamado dentro do método pagamentoProcessador

--- Processador.java
+++ Processador.java
@@ -11,10 +11,8 @@ public class Processador {
 	public boolean pagamentoProcessador(Boleto bol, Fatura fat) {
 		
 		double boleto = bol.getValorPago();
-		System.out.println("O valor da fatura era "+ fat.getValorFatura());
 		fat.setValorFatura(fat.getValorFatura()-boleto);
 		fat.verificaDivida();
-		System.out.println("O valor da fatura agora é: "+ fat.getValorFatura());
 		if (fat.pago == true) {
 			
 			System.out.println("A fatura está paga");
